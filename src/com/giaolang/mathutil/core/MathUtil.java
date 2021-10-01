ed5c81dac6fdef85011a0067e0e03df7fc5f6298
Khoinguyen107 <knguyen9047@gmail.com>
2021-10-01 10:28:25 +0700

    add recursion to see GREEN 4EVER again

--- src/com/giaolang/mathutil/core/MathUtil.java
+++ src/com/giaolang/mathutil/core/MathUtil.java
@@ -17,14 +17,29 @@ public class MathUtil {
     //TDD - Test Driven Development- viet test truoc khi viet code chinh
     //Test First Development, viet , suy nghi ve bo test caso/cac tinh huong
     //chay ham truoc khi viet ham
+//    public static long getFactorial(int n){
+//        if(n<0 || n>20)
+//            throw new IllegalArgumentException("n must be between 0 and 20");
+//        if(n==0 || n ==1)
+//            return 1;
+//        long product =1;
+//        for(int i=1;i<=n;i++)
+//            product *=i;
+//        return product;
+//    }
+    
+    
+    //Đệ quy, recursion, gọi lại chính mình với quy mô khác
+    //Búp bê người Nga
+    //5!= 1.2.3.4.5
+    //5!= 5*4!
+    //n!= n*(n-1)!công thức lặp lại chình mình
     public static long getFactorial(int n){
-        if(n<0 || n>20)
+        if(n<0||n>20)
             throw new IllegalArgumentException("n must be between 0 and 20");
-        if(n==0 || n ==1)
+        if(n==0 ||n==1)
             return 1;
-        long product =1;
-        for(int i=1;i<=n;i++)
-            product *=i;
-        return product;
+        return n*getFactorial(n-1);
+        
     }
 }
